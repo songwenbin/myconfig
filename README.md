44148adc8d915b4aeca2a3273acf605d4a365aa6
songwenbin <songwenbin@outlook.com>
2015-08-08 14:50:56 +0800

    add vim conf

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # My Config
 
 # Install List
+- build-essential
+- git
+- cmake
 - tmux
 - vim
+- gtags
 
-'''
+```
 # chmod 777 ./install.sh
 # ./install.sh
-'''
+```
--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 # Install Script
 
+#install enviroment
+sudo apt-get install build-essential
+sudo apt-get install git
+sudo apt-get install cmake
+
 #install tmux
 sudo apt-get install tmux
 cp ./tmux/.tmux.conf ~/
+
+#install vim
+sudo apt-get install vim
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+cp ./vim/.vimrc ~/
